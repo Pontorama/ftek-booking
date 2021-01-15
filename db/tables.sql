14ab0d3f96e27ac1cd7fd177ad56610d3d83c5e0
Eric Carlsson <32720134+ECarlsson@users.noreply.github.com>
2021-01-15 17:32:05 +0100

    various mysql-proprietary fixes

--- db/tables.sql
+++ db/tables.sql
@@ -1,33 +1,38 @@
 START TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS `users` (
-    `email` VARCHAR(100) PRIMARY KEY,
+    `email` VARCHAR(100),
     `name` VARCHAR(50) NOT NULL,
     `isAdmin` BOOLEAN DEFAULT FALSE NOT NULL,
-    `password` VARCHAR(255) NOT NULL
-) ENGINE=InnoDB;
+    `password` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`email`)
+);
 
 CREATE TABLE IF NOT EXISTS `rooms` (
-    `name` VARCHAR(100) PRIMARY KEY,
-    `manager` VARCHAR(100) REFERENCES `users`(`email`)
-) ENGINE=InnoDB;
+    `name` VARCHAR(100),
+    `manager` VARCHAR(100),
+    PRIMARY KEY (`name`),
+    CONSTRAINT `room_manager` FOREIGN KEY (`manager`) REFERENCES `users`(`email`)
+);
 
 CREATE TABLE IF NOT EXISTS `timeslots` (
     `id` INT UNSIGNED AUTO_INCREMENT,
-    `room` VARCHAR(100) REFERENCES `rooms`(`name`),
+    `room` VARCHAR(100),
     `from` TIME NOT NULL,
     `to` TIME NOT NULL,
-    `weekday` TINYINT NOT NULL CHECK (`weekday` >= 0 AND `weekday` <= 6),
-    PRIMARY KEY (`id`, `room`)
-) ENGINE=InnoDB;
+    `weekday` TINYINT NOT NULL,
+    CHECK (`weekday` >= 0 AND `weekday` <= 6),
+    PRIMARY KEY (`id`, `room`),
+    CONSTRAINT `room_timeslot` FOREIGN KEY (`room`) REFERENCES `rooms`(`name`) ON DELETE CASCADE
+);
 
 CREATE TABLE IF NOT EXISTS `inspection_times` (
     `time` TIME,
     `timeslot_id` INT UNSIGNED,
     `room` VARCHAR(100),
     PRIMARY KEY (`time`, `timeslot_id`, `room`),
-    FOREIGN KEY (`timeslot_id`, `room`) REFERENCES `timeslots`(`id`, `room`)
-) ENGINE=InnoDB;
+    CONSTRAINT `inspection_time_timeslot` FOREIGN KEY (`timeslot_id`, `room`) REFERENCES `timeslots`(`id`, `room`) ON DELETE CASCADE
+);
 
 CREATE TABLE IF NOT EXISTS `reservations` (
     `date` DATE,
@@ -40,8 +45,8 @@ CREATE TABLE IF NOT EXISTS `reservations` (
     `society` VARCHAR(50),
     `event` VARCHAR(100) NOT NULL,
     PRIMARY KEY (`date`, `timeslot_id`, `room`),
-    FOREIGN KEY (`timeslot_id`, `room`) REFERENCES `timeslots`(`id`, `room`),
-    FOREIGN KEY (`inspection_time`, `timeslot_id`, `room`) REFERENCES `inspection_times`(`time`, `timeslot_id`, `room`)
-) ENGINE=InnoDB;
+    CONSTRAINT `reservation_timeslot` FOREIGN KEY (`timeslot_id`, `room`) REFERENCES `timeslots`(`id`, `room`) ON DELETE CASCADE,
+    CONSTRAINT `reservation_inspection_time` FOREIGN KEY (`inspection_time`, `timeslot_id`, `room`) REFERENCES `inspection_times`(`time`, `timeslot_id`, `room`)
+);
 
 COMMIT;
