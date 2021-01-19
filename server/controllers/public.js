87c9760e011aa2bea2eb930a5ec0e55cf89bb5ed
Eric <32720134+ECarlsson@users.noreply.github.com>
2021-01-19 02:03:41 +0100

    fix params to query

--- server/controllers/public.js
+++ server/controllers/public.js
@@ -44,8 +44,8 @@ function getConfirmedReservationsForRoom(req, res, next) {
 }
 
 function createReservation(req, res, next) {
-  db.query('CALL `create_reservation`(?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    [req.body.date, req.body.timeslot, req.body.room, req.body.inspection_time, req.body.email, req.body.name, req.body.cid, req.body.society, req.body.description],
+  db.query('CALL `create_reservation`(?, ?, ?, ?, ?, ?, ?, ?)',
+    [new Date(req.body.date), req.body.timeslot, req.body.inspection_time, req.body.email, req.body.name, req.body.cid, req.body.society, req.body.description],
     err => {
       if (err)
         return next(err);
