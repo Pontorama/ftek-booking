60be66eaeae6b6307594f4f1ba2c7285b90cc3da
Eric <32720134+ECarlsson@users.noreply.github.com>
2021-01-20 02:13:33 +0100

    removed unused classname

--- src/views/RoomView.js
+++ src/views/RoomView.js
@@ -54,7 +54,7 @@ export default function RoomView({ roomId, roomName }) {
     <Container>
       <Row>
         <Col>
-          <Calendar className="calendar"
+          <Calendar 
             onClickDay={date => setActiveDate(date)}
             onActiveStartDateChange={handleActiveStartDateChange}
           />
