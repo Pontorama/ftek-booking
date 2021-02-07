adcd54316d70d9ba0b9e23cdde500f9ef5ebf043
Eric Carlsson <32720134+ECarlsson@users.noreply.github.com>
2021-02-07 17:36:11 +0100

    move api to /api

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const port = process.env.APP_PORT || 3000;
 const host = process.env.APP_HOST || 'localhost';
 const app = express();
 
-app.use('/', inspectionTimeRouter);
-app.use('/', reservationRouter);
-app.use('/', roomRouter);
-app.use('/', timeslotRouter);
-app.use('/', userRouter);
+app.use('/api', inspectionTimeRouter);
+app.use('/api', reservationRouter);
+app.use('/api', roomRouter);
+app.use('/api', timeslotRouter);
+app.use('/api', userRouter);
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
